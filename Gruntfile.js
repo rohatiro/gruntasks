5b1033a250ff6183fd957da51085fca6fae6934c
Roberto Tienda <rohatiro@gmail.com>
2018-04-03 09:13:15 -0500

    Definición de una multitarea

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,24 @@ module.exports = function(grunt) {
     grunt.initConfig({
         foo: {
             name: "hola"
+        },
+        multi: {
+            first: "1",
+            second: "2",
+            third: "3"
         }
     });
 
     //registerTask => function to define a basic task
     //registerTask("[name]", "[definition]", [task function]);
+    //default => This task will run automatically if grunt is executed without specifying any task
     grunt.registerTask("default", "tarea por defecto a ejecutar", function() {
         grunt.log.writeln("Hola Mundo");
+    });
+
+    //registerMultiTask => function to define a task that performs various tasks
+    //MultiTask needs the name of the task exist in the configuration by default, otherwise it will rise a exception
+    grunt.registerMultiTask("multi", "ejecución de varias tareas", function() {
+        grunt.log.writeln(this.target + ": " + this.data);
     });
 };
